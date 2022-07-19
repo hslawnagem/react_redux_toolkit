3e7b5c56b8371b0a12f3771e8411a8c25793433f
mew <megan.walsh@appliancesconnection.com>
2022-07-19 16:13:30 -0400

    NA

--- 01_lesson/src/app/store.js
+++ 01_lesson/src/app/store.js
@@ -1,8 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from '../features/counter/counterSlice';
+import {confifureStore } from "@reduxjs/toolkit"
 
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
+export const store = confifureStore(
+    {
+        reducer:{}
     }
-})
+)
